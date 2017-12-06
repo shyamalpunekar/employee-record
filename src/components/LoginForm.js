e4b07256ef43011bfd94eb62b516541569140a8d
origin <shyamal.punekar@gmail.com>
2017-12-06 15:21:50 -0800

    Add redux thunk to handle ajax

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { emailChanged } from '../actions';
+import { emailChanged, passwordChanged, loginUser } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
   onEmailChange(text){
     this.props.emailChanged(text);
+  }
+
+  onPasswordChanged(text){
+    this.props.passwordChanged(text);
+  }
+
+  onButtonPress(){
+    const { email, password } =this.props;
+    this.props.loginUser({ email, password });
   }
 
   render() {
@@ -22,13 +31,16 @@ class LoginForm extends Component {
 
         <CardSection>
         <Input
+          secureTextEntry
           label="Password"
           placeholder="password"
-          />
+          onChangeText={this.onPasswordChanged.bind(this)}
+          value={this.props.password}
+        />
         </CardSection>
 
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
           Login
           </Button>
         </CardSection>
@@ -39,8 +51,10 @@ class LoginForm extends Component {
 
 const mapStateToProps = state => {
   return {
-    email: state.auth.email
+    email: state.auth.email,
+    password: state.auth.password
   };
 };
 
-export default connect(mapStateToProps, { emailChanged }) (LoginForm);
+export default connect(mapStateToProps,
+  { emailChanged, passwordChanged, loginUser }) (LoginForm);
